aea4477140648392b704836cfbb3f12d935b1691
FrigerioGianfranco <157016529+FrigerioGianfranco@users.noreply.github.com>
2024-06-18 16:54:05 +0900

    Making sure all dependecies are loaded

---
+++ R/zzz.R
@@ -0,0 +1,13 @@
+# File: R/zzz.R
+
+#' @import tidyverse
+#' @import ggpubr
+#' @import ggrepel
+#' @import lmerTest
+#' @import AER
+#' @import MuMIn
+#' @import writexl
+#' @import xlsx
+#' @import gridExtra
+
+NULL
